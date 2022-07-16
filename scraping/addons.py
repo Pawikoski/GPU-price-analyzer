d669912fd7fdf3231a520d191ed16879f98b4124
Pawikoski <pawikoski@gmail.com>
2022-07-16 15:36:09 +0200

    New function: (GPU) Brand detector
    Took 1 hour 53 minutes

---
+++ scraping/addons.py
@@ -0,0 +1,11 @@
+BRANDS = {"asus", "gigabyte", "inno3d", "palit", "gainward", "aorus", "pny", "zotac", "kfa2", "msi", "biostar", "afox",
+          "asrock", "evga", "sapphire technology", "sapphire", "xfx", "powercolor"}
+
+
+def brand_detector(name: str) -> str | bool:
+    name = name.lower()
+    for brand in BRANDS:
+        if brand in name:
+            return brand
+
+    return False
